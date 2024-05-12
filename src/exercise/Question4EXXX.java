4b0a2e69f9dddabc75f1c99fbab998f14fcbb548
MiskuZero <739740010@qq.com>
2024-05-13 03:43:24 +0800

    day08 Question4EXXX：终极优化方案 - DFS+剪枝

--- src/exercise/Question4EXXX.java
+++ src/exercise/Question4EXXX.java
@@ -9,31 +9,38 @@ public class Question4EXXX {
         ArrayList<int[]> edges = GraphUtil.edges;
 
         long t = System.currentTimeMillis();
-        dfs(0, v, new int[v + 1], 1, edges);
+        dfs(new int[v + 1], 1, v + 1, edges);
         System.out.println("（EXXX）红色节点不相邻的情况总数：" + count);
         System.out.println("（EXXX）用时：" + (System.currentTimeMillis() - t) + "ms");
     }
 
     static int count;
 
-    public static void dfs(int index, int end, int[] nums, int size, ArrayList<int[]> edges) {
+    public static void dfs(int[] nums, int index, int end, ArrayList<int[]> edges) {
         if (index == end) {
             //处理
             boolean test = true;
-            for (int[] edge : edges) {
+            for (int[] edge : edges)
                 if (nums[edge[0]] == 1 && nums[edge[1]] == 1) {
                     test = false;
                     break;
                 }
-            }
             if (test)
                 count++;
             return;
         }
-        for (int i = 0; i < 2; i++) {
-            nums[size++] = i;
-            dfs(index + 1, end, nums, size, edges);
-            size--;
+        branch:
+        for (int i = 1; i > -1; i--) {
+            nums[index++] = i;
+            if (index > 2) //剪枝
+                for (int[] edge : edges) {
+                    int e0 = edge[0], e1 = edge[1];
+                    if ((e0 < index && e1 < index) && (nums[e0] == 1 && nums[e1] == 1)) {
+                        index--;
+                        continue branch;
+                    }
+                }
+            dfs(nums, index--, end, edges);
         }
     }
 }
